816c937a19ce7201db8f68ecb8621697cb6d3fc7
Tobias <Suchty112@users.noreply.github.com>
2019-05-01 22:45:09 +0200

    verbamdfester

--- modules/lss-layout-nacht-02/layout-nacht-02.user.js
+++ modules/lss-layout-nacht-02/layout-nacht-02.user.js
@@ -3,8 +3,14 @@
 
     $('body').prepend(
         '<style type="text/css" rel="stylesheet" id="lss-layout-nacht-02-style">' +
-        '.well{background-image:none !important}div[id$="_outer"] .btn-default:focus,div[id$="_outer"] .btn-default:hover {color:#ee212a} div[id$="_outer"] .btn-default {background-color:#1d212a !important;color:#b9b9b9} .input-group-addon,.modal-content {border:0; background-color:#13151b}.table-striped>tbody>tr:nth-of-type(odd),.lightbox_iframe,.dropdown-menu>li>a{border:0;background-color:#191F3A!important;color:#b9b9b9}.panel-heading, .panel-footer{background-color:#1d212a !important; color:#b9b9b9!important}.navbar-default,.input-group-addon,.missionSideBarEntry,.panel-body{background-color:#13151b!important;color:#b9b9b9!important}#lost-menu a,.well,.dropdown-menu,.nav-tabs>li>a {color:#f5f5f5;background-color:#1d212a!important}.map_position_mover{color:#b9b9b9!important}' +
-        'html,body,#missions_outer,#buildings_outer,#chat_outer,#radio_outer,#bigMapWindow,#lightbox_box,.close{background-color:#0a0b0e !important;color:#b9b9b9}html,body.dark,#missions_outer,#buildings_outer,#chat_outer,#radio_outer,#bigMapWindow,#lightbox_box,.close{background-color:#0a0b0e !important;color:#b9b9b9}input,textarea{background-color:#151a31!important;border:0} .progress {background-color: #486683!important}' +
+        '.well{background-image:none !important}div[id$="_outer"] .btn-default:focus,div[id$="_outer"] .btn-default:hover {color:#ee212a} div[id$="_outer"] ' +
+        '.btn-default {background-color:#1d212a !important;color:#b9b9b9} .input-group-addon,.modal-content {border:0; background-color:#13151b}.table-striped>tbody>tr:nth-of-type(odd),' +
+        '.lightbox_iframe,.dropdown-menu>li>a{border:0;background-color:#191F3A!important;color:#b9b9b9}.panel-heading, .panel-footer{background-color:#1d212a !important; color:#b9b9b9!important}' +
+        '.navbar-default,.input-group-addon,.missionSideBarEntry,.panel-body{background-color:#13151b!important;color:#b9b9b9!important}#lost-menu a,.well,.dropdown-menu,.nav-tabs>li>a {color:#f5f5f5;background-color:#1d212a!important}.map_position_mover{color:#b9b9b9!important}' +
+        '.navbar navbar-default{background-color:#13151b!important;color:#b9b9b9!important}' +
+        'html,body,#missions_outer,#buildings_outer,#chat_outer,#radio_outer,#bigMapWindow,#lightbox_box,.close{background-color:#0a0b0e !important;color:#b9b9b9}' +
+        'html,body.dark,#missions_outer,#buildings_outer,#chat_outer,#radio_outer,#bigMapWindow,#lightbox_box,.close{background-color:#0a0b0e !important;color:#b9b9b9}' +
+        'input,textarea{background-color:#151a31!important;border:0} .progress {background-color: #486683!important}' +
         '</style>'
     );
 })();
